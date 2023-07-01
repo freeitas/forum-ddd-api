38e2ac4d029ebeab157b529b73b20347560d2079
Guilherme Freitas <hfdsguilherme@gmail.com>
2023-07-01 08:36:07 -0300

    feat: list recent questions usecase

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,7 +1,9 @@
 import { Question } from "@/domain/forum/enterprise/entities/question";
+import { PaginationParams } from "@/core/repositories/pagination-params";
 
 export interface QuestionsRepository {
   findById(id: string): Promise<Question | null>;
+  findManyRecent(params: PaginationParams): Promise<Question[]>;
   create(question: Question): Promise<void>;
   save(question: Question): Promise<void>;
   findBySlug(slug: string): Promise<Question | null>;
